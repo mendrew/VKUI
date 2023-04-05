f5682ad52912804da3d75a86598229061352c4b5
Evgeniya <e.polozova@corp.vk.com>
2023-04-05 15:45:26 +0300

    fix(ChipsInput,ChipsSelect): combobox role

--- packages/vkui/src/components/ChipsSelect/ChipsSelect.tsx
+++ packages/vkui/src/components/ChipsSelect/ChipsSelect.tsx
@@ -331,7 +331,7 @@ export const ChipsSelect = <Option extends ChipOption>(props: ChipsSelectProps<O
           className,
         )}
         disabled={disabled}
-        role="application"
+        role="combobox"
         aria-disabled={disabled}
         aria-readonly={restProps.readOnly}
         after={
