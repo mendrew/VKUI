7b3288b6fc01cd2714dff3e8406387cbe7383d4f
Andrey Medvedev <andr.medv.spb@gmail.com>
2023-07-21 14:33:35 +0300

    Revert "Fix search placeholder (#5490) (#1)"
    This reverts commit 61b614b65f5a0a5344c2761745839fb4d4ddf298.

--- packages/vkui/src/components/Search/Search.tsx
+++ packages/vkui/src/components/Search/Search.tsx
@@ -112,7 +112,6 @@ export const Search = ({
         value && styles['Search--has-value'],
         after && styles['Search--has-after'],
         icon && styles['Search--has-icon'],
-        inputProps.disabled && styles['Search--disabled'],
         className,
       )}
       style={style}
